28297cf4dab3f506727455c93089022000102bbd
Joshua <joshua.becchiati04@gmail.com>
2025-10-01 09:59:05 +0200

    Combat buttons, Start and End player turn

--- Assets/Scripts/Managers/NewBattleManager.cs
+++ Assets/Scripts/Managers/NewBattleManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -14,16 +14,24 @@ public class NewBattleManager : MonoBehaviour
     [Header("Battle List")]
     [SerializeField] private List<UnitBase> _unitsInBattle;
 
+    // --- Instance ---
     public static NewBattleManager Instance { get; private set; }
 
+    // --- Private ---
     private int _indexTarget = 0;
     private int _indexCurrentUnit = 0;
+    private int _oldTarget = 0;
 
     private bool _isPlayerActing = false;
 
     private UnitBase _currentUnit;
     private Camera _battleCamera;
 
+    // --- Events ---
+    public event Action<ItemData> OnUseItem;
+    public event Action<AbilityData> OnUseAbility;
+
+    #region Unity Methods
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -45,7 +53,12 @@ public class NewBattleManager : MonoBehaviour
         if (PlayerInputSingleton.Instance != null)
             PlayerInputSingleton.Instance.Actions["Combat"].performed -= SelectAttackTarget;
     }
+    #endregion
 
+    #region Setup
+    /// <summary>
+    /// Setup all the mandatory needs for the battle to start
+    /// </summary>
     public void SetupBattle(BattleSettings battleSettings, IReadOnlyList<GameObject> playersPf)
     {
         _battleCamera = Camera.main;
@@ -62,7 +75,7 @@ public class NewBattleManager : MonoBehaviour
         {
             GameObject go = Instantiate(battleSettings.enemies[i], _spawnPointsEnemies[i]);
             go.TryGetComponent(out EnemyInCombat e);
-            e.SetSpeed(Random.Range(10, 20));
+            e.SetSpeed(UnityEngine.Random.Range(10, 20));
             _unitsInBattle.Add(e);
         }
 
@@ -73,6 +86,7 @@ public class NewBattleManager : MonoBehaviour
         _currentUnit = _unitsInBattle[0];
         _currentUnit.StartTurn();
     }
+    #endregion
 
     private void SelectAttackTarget(InputAction.CallbackContext context)
     {
@@ -99,15 +113,111 @@ public class NewBattleManager : MonoBehaviour
     private void ChangeTurn()
     {
         _currentUnit.EndTurn();
+        if (_currentUnit.Team == EUnitTeam.player)
+            EndPlayerTurn();
 
-        do
-        {
-            _indexCurrentUnit += 1;
-        } while (_unitsInBattle[_indexCurrentUnit].Health <= 0);
-
+        do _indexCurrentUnit += 1;
+        while (_unitsInBattle[_indexCurrentUnit].Health <= 0);
 
         _currentUnit = _unitsInBattle[_indexCurrentUnit];
 
+        if (_currentUnit.Team == EUnitTeam.player)
+            StartPlayerTurn();
         _currentUnit.StartTurn();
     }
+
+    #region Buttons Logic
+    /// <summary>
+    /// Attack the selected enemy
+    /// </summary>
+    public void BTNAttack()
+    {
+        if (_isPlayerActing)
+        {
+            _isPlayerActing = false;
+            void HandleEndAttack()
+            {
+                _currentUnit.OnEndAttack -= HandleEndAttack;
+                EndPlayerTurn();
+            }
+
+            _currentUnit.OnEndAttack += HandleEndAttack;
+            _currentUnit.StartAttackAnimation(_unitsInBattle[_indexTarget]);
+        }
+    }
+
+    /// <summary>
+    /// Skip the turn of the current player
+    /// </summary>
+    public void BTNSkipTurn()
+    {
+        if (_isPlayerActing)
+        {
+            _isPlayerActing = false;
+            EndPlayerTurn();
+        }
+    }
+
+    public void BTNEscape()
+    {
+        if (_isPlayerActing)
+        {
+            _isPlayerActing = false;
+            GameEvents.BattleEnd();
+        }
+    }
+
+    /// <summary>
+    /// Use item logic
+    /// </summary>
+    /// <param name="item"></param>
+    public void BTNUseItem(ItemData item)
+    {
+        if (_isPlayerActing)
+        {
+            item.UseItem(_unitsInBattle[_indexTarget]);
+            OnUseItem?.Invoke(item);
+            EndPlayerTurn();
+        }
+    }
+
+    /// <summary>
+    /// Use ability logic.
+    /// The ability can't be use if is not completely charge
+    /// </summary>
+    /// <param name="ability"></param>
+    public void BTNUseAbility(AbilityData ability)
+    {
+        if (_isPlayerActing && ability.ChargeCounter == ability.Ability.maxCharge)
+        {
+            _isPlayerActing = false;
+            List<UnitBase> enemies = new() { _unitsInBattle[_indexTarget] };
+            enemies.AddRange(_unitsInBattle.FindAll(e => e.Team == EUnitTeam.enemy && e != _unitsInBattle[_indexTarget]));
+            ability.UseAbility(enemies.ToArray());
+            OnUseAbility?.Invoke(ability);
+            ChangeTurn();
+        }
+    }
+    #endregion
+
+    #region Turn management
+    private void StartPlayerTurn()
+    {
+        _isPlayerActing = true;
+        _indexTarget = _oldTarget;
+    }
+
+    private void EndPlayerTurn()
+    {
+        _isPlayerActing = false;
+
+        // Check if the current target is alive and is an enemy.
+        // If yes, store the index for the next turn
+        // else search the first alive enemy index
+        if (_unitsInBattle[_indexTarget].Health > 0 && _unitsInBattle[_indexTarget].Team == EUnitTeam.enemy)
+            _oldTarget = _indexTarget;
+        else
+            _oldTarget = _unitsInBattle.FindIndex(e => e.Team == EUnitTeam.enemy && e.Health > 0);
+    }
+    #endregion
 }
